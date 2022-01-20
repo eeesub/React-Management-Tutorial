b71df072b552336c42d7f89faaa92c5b9c6ef791
eeesub <eeesub@gmail.com>
2022-01-20 17:06:22 +0900

    Customer REST API

--- client/src/App.js
+++ client/src/App.js
@@ -20,35 +20,26 @@ const styles = theme => ({
     minWidth: 1080  
   }
 })
-const customers = [ 
-  {
-    id: 1,
-    image: 'https://placeimg.com/64/64/1',
-    name: '홍길동',
-    birthday: '651210',
-    gender: '남자',
-    job: '대학생'
-  }, 
-  {
-    id: 2,
-    image: 'https://placeimg.com/64/64/2',
-    name: '임꺽정',
-    birthday: '651210',
-    gender: '남자',
-    job: '대학생'
-  }, 
-  {
-    id: 3,
-    image: 'https://placeimg.com/64/64/3',
-    name: '저팔계',
-    birthday: '651210',
-    gender: '남자',
-    job: '대학생'
-  }, 
-]
 
-function App(p) {
-  const { classes } = p;
+class App extends React.Component {
+  state = {
+    customers:""
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers:res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async() => {
+    const response = await fetch('/api/customers')
+    const body= await response.json()
+    return body
+  }
+
+  render(p) {
+  const { classes } = this.props;
   return (
     <Paper className = {classes.root}>
       <Table className = {classes.table}>
@@ -63,14 +54,15 @@ function App(p) {
           </TableRow>
         </TableHead>
         <TableBody>
-          { customers.map(c => { return(
+          {this.state.customers ?  this.state.customers.map(c => { return(
             <Customer key= {c.id} id = {c.id} image = {c.image}  name = {c.name} 
               birthday = {c.birthday}  gender = {c.gender}   job = {c.job}    /> 
             )} ) 
-          }
+          :" "}
         </TableBody>
       </Table>   
     </Paper>)
+  }
 }
 
 export default withStyles(styles)(App)
--- server.js
+++ server.js
@@ -4,11 +4,38 @@ const { application } = require("express");
 const app = express()
 const port = process.env.PORT || 5000
 
+const customers = [ 
+    {
+      id: 1,
+      image: 'https://placeimg.com/64/64/1',
+      name: '홍길동',
+      birthday: '651210',
+      gender: '남자',
+      job: '대학생'
+    }, 
+    {
+      id: 2,
+      image: 'https://placeimg.com/64/64/2',
+      name: '임꺽정',
+      birthday: '651210',
+      gender: '남자',
+      job: '대학생'
+    }, 
+    {
+      id: 3,
+      image: 'https://placeimg.com/64/64/3',
+      name: '저팔계',
+      birthday: '651210',
+      gender: '남자',
+      job: '대학생'
+    }, 
+  ]
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.get('/api/hello', (req, res) => {
-    res.send({message:'Hello Express' })
+app.get('/api/customers', (req, res) => {
+    res.send(customers)
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`) )
