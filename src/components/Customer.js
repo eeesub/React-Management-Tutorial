2ade7f45781ed80a059d277bcf9a00332be08fc2
eeesub <eeesub@gmail.com>
2022-01-20 14:10:20 +0900

    Apply MD

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,29 +1,16 @@
+import React from 'react'
 import TableRow from '@material-ui/core/TableRow'
 import TableCell from '@material-ui/core/TableCell'
 export default function Customer(p) {
     return (
-        <div>
-            <CustomerProfile id={p.id} name={p.name} image= {p.image} />
-            <CustomerInfo birthday={p.birthday} gender={p.gender} job={p.job}/>
-        </div>
+        <TableRow>
+            <TableCell>{p.id}</TableCell>
+            <TableCell><img src={p.image} alt="pic"/></TableCell>
+            <TableCell>{p.name}</TableCell>
+            <TableCell>{p.gender}</TableCell>
+            <TableCell>{p.birthday}</TableCell>
+            <TableCell>{p.job}</TableCell>
+        </TableRow>
     )
 } 
-
-export function CustomerProfile(p) {
-    return (
-        <div>
-            <img src={p.image} alt='profile' />
-            <h2>{p.name}({p.id})</h2>
-        </div>
-    )
-}
-
-function CustomerInfo(p) {
-    return(
-        <div>
-            <p>{p.birthday}</p>
-            <p>{p.gender}</p>
-            <p>{p.job}</p>
-        </div>
-    )
-}
+ 
